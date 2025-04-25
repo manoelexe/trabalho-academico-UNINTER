5166962836d9d244bc73ca6effed150398f0013e
Manuel Richard <107644690+manoelexe@users.noreply.github.com>
2025-04-25 14:37:18 -0300

    Update MapsrecicleApplication.java

--- src/main/java/com/IPSEI/mapsrecicle/MapsrecicleApplication.java
+++ src/main/java/com/IPSEI/mapsrecicle/MapsrecicleApplication.java
@@ -2,3 +2,13 @@ package com.IPSEI.mapsrecicle;
 
 
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapsrecicleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MapsrecicleApplication.class, args);
+    }
+}
